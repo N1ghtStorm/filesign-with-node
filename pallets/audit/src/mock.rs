9ef22e98c79e04c3e7429220a8d757a5a827c005
N1ghtStorm <bushuevdv@mail.ru>
2021-08-28 11:21:39 +0300

    add test mock

--- pallets/audit/src/mock.rs
+++ pallets/audit/src/mock.rs
@@ -1,4 +1,4 @@
-use crate as pallet_template;
+use crate as pallet_audit;
 use sp_core::H256;
 use frame_support::parameter_types;
 use sp_runtime::{
@@ -16,15 +16,10 @@ frame_support::construct_runtime!(
 		NodeBlock = Block,
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+		System: frame_system::{Pallet, Call, Config, Storage},
+		Audit: pallet_audit::{Pallet, Call, Storage},
 	}
 );
-
-parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const SS58Prefix: u8 = 42;
-}
 
 impl system::Config for Test {
 	type BaseCallFilter = ();
@@ -40,20 +35,19 @@ impl system::Config for Test {
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = BlockHashCount;
+	type Event = ();
+	type BlockHashCount = ();
 	type Version = ();
 	type PalletInfo = PalletInfo;
 	type AccountData = ();
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
-	type SS58Prefix = SS58Prefix;
+	type SS58Prefix = ();
 	type OnSetCode = ();
 }
 
-impl pallet_template::Config for Test {
-	type Event = Event;
+impl pallet_audit::Config for Test {
 }
 
 // Build genesis storage according to the mock runtime.
