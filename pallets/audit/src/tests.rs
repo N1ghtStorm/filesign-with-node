eb037ec024c64f32145cd5f184705553381417c6
N1ghtStorm <bushuevdv@mail.ru>
2021-08-30 13:23:39 +0300

    refactor + add todo

--- pallets/audit/src/tests.rs
+++ pallets/audit/src/tests.rs
@@ -2,37 +2,14 @@ use crate::{Error, mock::*};
 use frame_support::{assert_ok, assert_noop};
 
 
-//type Audit = Module<Test>;
-
 #[test]
-fn it_works_for_default_value() {
-	// new_test_ext().execute_with(|| {
-	// 	// Dispatch a signed extrinsic.
-	// 	// assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-	// 	// // Read pallet storage and assert an expected result.
-	// 	// assert_eq!(TemplateModule::something(), Some(42));
-	// });
-}
-
-
-#[test]
-fn it_works_for_file_create() {
-	// new_test_ext().execute_with(|| {
-	// 	// Dispatch a signed extrinsic.
-	// 	// assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-	// 	// // Read pallet storage and assert an expected result.
-	// 	// assert_eq!(TemplateModule::something(), Some(42));
-	// });
-
+fn it_works_for_create_new_file() {
 	new_test_ext().execute_with(|| {
 		//Dispatch a signed extrinsic.
-		//let tag = Vec::<u8>::new();
 		let tag = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 		let filehash = 666_666_u64;
 
 		assert_ok!(Audit::create_new_file(Origin::signed(1) ,tag, filehash), ());
-
-		// Read pallet storage and assert an expected result.
-		//assert_eq!(TemplateModule::something(), Some(42));
+		todo!("add storage tests");
 	});
 }
